d39f3774b3e5f72d28f67eaf4e5d1476457c9712
Aman Tripathi <amancselover@gmail.com>
2022-11-13 00:43:10 +0530

    Domestica - 2

--- src/containers/register/Register.jsx
+++ src/containers/register/Register.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './register.css'
 
 function Register() {
     return (
@@ -10,8 +10,35 @@ function Register() {
             </div>
 
             <div className="gpt3__header-image">
-                
-                
+
+
+                <form>
+                    <div className="form-group row m-2">
+                        <label htmlFor="name" className='col-sm-4'><h5 className='text-white'>Name</h5></label>
+                        <div className='col-sm-8'>
+                            <input type="name" className='form-control' id="name" />
+                        </div>
+                    </div>
+                    <div className="form-group row m-2" >
+                        <label htmlFor="contact" className='col-sm-4'><h5 className='text-white'>Contact</h5></label>
+                        <div className='col-sm-8'>
+                            <input type="contact" className="form-control" id="contact"/>
+                        </div>
+                    </div>
+                    <div className="form-group row m-2" >
+                        <label htmlFor="password" className='col-sm-4'><h5 className='text-white'>Password</h5></label>
+                        <div className='col-sm-8'>
+                            <input type="password" className="form-control" id="password" />
+                        </div>
+                    </div>
+                    <div className="form-group row m-2" >
+                        <label htmlFor="dob" className='col-sm-4'><h5 className='text-white'>Birth Date</h5></label>
+                        <div className='col-sm-8'>
+                            <input type="date" className="form-control" id="dob"/>
+                        </div>
+                    </div>
+                </form>
+
 
             </div>
         </div>
